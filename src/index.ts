fe5a16bcc8fa855596ca2acbbe45fd2419978575
Kostiantyn Nahula <kostynnagula@gmail.com>
2025-03-15 15:46:13 +0200

    Implemented translation function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,30 @@
+import { ChatOpenAI } from '@langchain/openai';
+import { ChatPromptTemplate } from '@langchain/core/prompts';
+
 import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log("Langchain project is running...");
+const translation = async (text: string, language: string) => {
+  console.log('Run the Langchain project...');
+
+  const model = new ChatOpenAI({ model: "gpt-4" });
+
+  const systemTemplate = "Translate the following from English into {language}";
+
+  const promptTemplate = ChatPromptTemplate.fromMessages([
+    ["system", systemTemplate],
+    ["user", "{text}"],
+  ]);
+
+  const promptValue = await promptTemplate.invoke({
+    language,
+    text,
+  });
+
+  const response = await model.invoke(promptValue);
+
+  console.log(response)
+}
+
+// translation("Hello, how are you?", "French");
