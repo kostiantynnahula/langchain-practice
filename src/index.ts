11024d685542370d5544387c408adeeec660c04c
Kostiantyn Nahula <kostynnagula@gmail.com>
2025-03-20 20:15:05 +0200

    Created agent

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const run = async () => {
-  
+
+  console.log('Run the Langchain project start...');
+
+  // Define the tools for the agent to use
+
   console.log('Run the Langchain project end...');  
 };
 
